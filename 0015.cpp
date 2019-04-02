e5ee5246ed51ba7cd82862010203da64e1367d67
liuke <liuke@mail.bafst.com>
2019-04-02 08:50:59 +0800

    15. 3Sum【使用二分略微优化】

--- 0015.cpp
+++ 0015.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
     vector< vector<int> > threeSum(vector<int>& nums) {
-    	int len = nums.size();
+    	int len = nums.size(), i = 0;
     	//œ»≈≈–Ú 
     	sort(nums.begin(), nums.end());
     	
         vector< vector<int> > result;
-        for(int i = 0; i < len; ++ i) {
+        while (i < len) {
         	int target = - nums[i], j = i + 1, k = len - 1;
 			while (j < k) {
 				int current = nums[j] + nums[k];
@@ -25,14 +25,14 @@ public:
 					while(j < k && nums[j] == left) {
 						++ j;
 					}
-                    while(j < k && nums[k] == right) {
-                    	-- k;
+					while(j < k && nums[k] == right) {
+						-- k;
 					}
 				}
 			}
-			while(i + 1 < len && nums[i + 1] == nums[i]) {
-				++ i;
-			}
+			int count = upper_bound(nums.begin(), nums.end(), nums[i]) - 
+			lower_bound(nums.begin(), nums.end(), nums[i]);
+			i += count;
 		}
         return result;
     }
